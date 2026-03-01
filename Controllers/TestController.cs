491c58fc8ecbdd42301bd176adf47307fc5b6990
David Duarte <141060732+daviddev98@users.noreply.github.com>
2026-02-28 22:37:38 -0600

    test David

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -12,7 +12,7 @@ namespace ProyectoS4.Controllers
 
     public class TestController : Controller
     {      
-        //prueba
+        //prueba David
      
             private readonly FirebaseService _firebaseService;
             private readonly ILogger<TestController> _logger;       
