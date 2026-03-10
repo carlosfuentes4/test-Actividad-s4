5c7d6743eb9ff02a7d6ee15937905cd0e7cf5a63
joseblog77 <70163423+joseblog77@users.noreply.github.com>
2026-03-09 21:54:49 -0600

    Creacion de 2 Services
    asdasdadapwdas

---
+++ Services/IRoomService.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoS4.DTOs;
+using ProyectoS4.Models;
+
+namespace ProyectoS4.Services;
+
+public interface IRoomService
+{
+    Task<List<RoomDto>> GetAllRooms();
+    Task<RoomDto?> GetRoomById(string roomId);
+    Task<RoomModel> CreateRoom(RoomDto dto);
+    Task<RoomModel> UpdateRoom(string roomId, RoomDto dto);
+    Task DeleteRoom(string roomId);
+}
---
+++ Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoS4.DTOs;
+using ProyectoS4.Models;
+
+namespace ProyectoS4.Services;
+
+public interface IUserService
+{
+    Task<List<UserDto>> GetAllUsers();        
+    // GET todos
+    Task<UserDto?> GetUserById(string userId); 
+    // GET por id
+    Task<UserModel> CreateUser(UserDto dto);   
+    // POST crear
+    Task<UserModel> UpdateUser(string userId, UserDto dto); 
+    // PUT actualizar
+    Task DeleteUser(string userId);            
+    // DELETE eliminar
+}
