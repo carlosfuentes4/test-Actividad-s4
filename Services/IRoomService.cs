5511d0013ea0e176b20a06de5bd76b4dc464f4f2
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-03-09 22:13:27 -0600

    room interface

---
+++ Services/IRoomService.cs
@@ -0,0 +1,14 @@
+﻿namespace ProyectoS4.Services
+using ProyectoS4.DTO;
+using ProyectoS4.Services;
+{
+    public interface IRoomService
+    {
+    Task<List<RoomDto>> GetRooms();
+    Task<RoomDto?> GetRoomById(string roomId);
+    Task<Room> CreateRoom(Room room);
+    Task<Room> UpdateRoom(string roomId, Room room);
+    Task DeleteRoom(string roomId);
+
+}
+}
