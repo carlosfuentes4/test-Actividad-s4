473925b91e33757cc27323bf220573f560f4ad0c
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-03-13 21:40:45 -0600

    update iroomservice

--- Services/IRoomService.cs
+++ Services/IRoomService.cs
@@ -1,14 +1,17 @@
-﻿namespace ProyectoS4.Services
-using ProyectoS4.DTO;
-using ProyectoS4.Services;
-{
-    public interface IRoomService
-    {
-    Task<List<RoomDto>> GetRooms();
-    Task<RoomDto?> GetRoomById(string roomId);
-    Task<Room> CreateRoom(Room room);
-    Task<Room> UpdateRoom(string roomId, Room room);
-    Task DeleteRoom(string roomId);
+﻿using ProyectoS4.DTO;
+using ProyectoS4.Models;
 
-}
+namespace ProyectoS4.Services;
+
+public interface IRoomService
+{
+    Task<List<HabitacionDto>> GetRooms();
+
+    Task<HabitacionDto?> GetRoomById(string roomId);
+
+    Task<RoomModel> CreateRoom(RoomModel room);
+
+    Task<RoomModel> UpdateRoom(string roomId, RoomModel room);
+
+    Task DeleteRoom(string roomId);
 }
